ea373f54868309c6629212ad4cf498858e20086c
James Clarke (WDG) <jeclarke@ntdev.microsoft.com>
2017-10-12 08:56:21 -0700

    versioning

--- buildbinandnuget.bat
+++ buildbinandnuget.bat
@@ -1,3 +1,3 @@
 msbuild AudioVisualizer.sln /t:build /p:Configuration=Release;Platform=x86
 msbuild AudioVisualizer.sln /t:build /p:Configuration=Release;Platform=x64
-Tools\nuget.exe pack AudioAnalyzer.nuspec -o .\package
+Tools\nuget.exe pack AudioAnalyzer.nuspec -o .\package -Version 0.0.5
