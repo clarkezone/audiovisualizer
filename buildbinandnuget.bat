34dad3d3ff3f4deb8ea75b39ef14ae9e396965dc
James Clarke <james@clarkezone.net>
2017-10-19 10:04:27 -0700

    attempt to fix certs

--- buildbinandnuget.bat
+++ buildbinandnuget.bat
@@ -1,3 +1,13 @@
-msbuild AudioVisualizer.sln /t:build /p:Configuration=Release;Platform=x86
-msbuild AudioVisualizer.sln /t:build /p:Configuration=Release;Platform=x64
-Tools\nuget.exe pack AudioAnalyzer.nuspec -o .\package -Version %1
+@echo off
+msbuild AudioVisualizer.sln /t:build /p:Configuration=Release;Platform=x86 && (
+	msbuild AudioVisualizer.sln /t:build /p:Configuration=Release;Platform=x64 && (
+		Tools\nuget.exe pack AudioAnalyzer.nuspec -o .\package -Version %1 && (
+			echo "Package created"
+		) || ( 
+			echo "x64 build failed"
+		)
+	)
+
+) || (
+	echo "x86 build failed"
+)
