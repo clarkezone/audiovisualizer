de22f73fe7be0686b88a9467bf3f1f47df669530
Tõnu Vanatalu <tonuv@hotmail.com>
2018-05-24 12:29:56 +0300

    SourceConverter code and tests

--- AudioVisualizer.test/SourceConverterTests.cs
+++ AudioVisualizer.test/SourceConverterTests.cs
@@ -8,6 +8,7 @@ using AudioVisualizer;
 using System.ComponentModel;
 using Windows.Foundation;
 using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace AudioVisualizer.test
 {
@@ -124,48 +125,389 @@ namespace AudioVisualizer.test
             TestProperty(ScaleType.Linear, "FrequencyScale");
         }
 
+        [TestMethod]
+        [TestCategory("SourceConverter")]
+        public void SourceConverter_Property_AnalyzerTypes()
+        {
+            sut.AnalyzerTypes = AnalyzerType.Spectrum;
+            Assert.AreEqual(AnalyzerType.Spectrum, sut.AnalyzerTypes);
+            CollectionAssert.AreEqual(new string[] { "AnalyzerTypes" }, propertiesChanged, "Change notification missing assigning AnalyzerTypes property");
+            sut.AnalyzerTypes = AnalyzerType.Spectrum;
+            CollectionAssert.AreEqual(new string[] { "AnalyzerTypes" }, propertiesChanged, "Invalid change notification when assigning same value to AnalyzerTypes");
+        }
+
+        [TestMethod]
+        [TestCategory("SourceConverter")]
+        public void SourceConverter_Property_Source()
+        {
+            FakeVisualizationSource fakeSource = new FakeVisualizationSource();
+            sut.Source = fakeSource;
+            Assert.AreEqual(fakeSource, sut.Source);
+            CollectionAssert.AreEqual(new string[] { "Source" }, propertiesChanged,"Change notification missing assigning Source property");
+            sut.Source = fakeSource;
+            CollectionAssert.AreEqual(new string[] { "Source" }, propertiesChanged,"Invalid change notification when assigning same value to Source");
+            sut.Source = null;
+            CollectionAssert.AreEqual(new string[] { "Source","Source" }, propertiesChanged, "Change notification missing assigning null to Source property");
+            sut.Source = null;
+            CollectionAssert.AreEqual(new string[] { "Source", "Source" }, propertiesChanged, "Invalid change notification when assigning same value (null) to Source");
+        }
 
 
     }
 
-
-
-    public class SourceConverterTests
+    [TestClass]
+    public class SourceConverterPropertyValidationWithSourceSet
     {
+        SourceConverter sut;
+        FakeVisualizationSource _source;
+        [TestInitialize]
+        public void TestInit()
+        {
+            sut = new SourceConverter();
+            _source = new FakeVisualizationSource();
+            sut.Source = _source;
+        }
+
+        [TestMethod]
+        [TestCategory("SourceConverter")]
+        public void SourceConverter_IsFpsPassedThrough()
+        {
+            Assert.AreEqual(_source.Fps, sut.Fps);
+        }
+        [TestMethod]
+        [TestCategory("SourceConverter")]
+        public void SourceConverter_IsPlaybackStatePassedThrough()
+        {
+            Assert.AreEqual(_source.PlaybackState, sut.PlaybackState);
+            _source.State = SourcePlaybackState.Playing;
+            Assert.AreEqual(_source.State, sut.PlaybackState);
+        }
+        [TestMethod]
+        [TestCategory("SourceConverter")]
+        public void SourceConverter_IsSuspendedPassedThrough()
+        {
+            Assert.AreEqual(_source.IsSuspended, sut.IsSuspended);
+            _source.IsSuspended = !_source.IsSuspended;
+            Assert.AreEqual(_source.IsSuspended, sut.IsSuspended);
+        }
+        [TestMethod]
+        [TestCategory("SourceConverter")]
+        public void SourceConverter_IsPresentationTimePassedThrough()
+        {
+            Assert.AreEqual(_source.PresentationTime, sut.PresentationTime);
+            _source.Time = TimeSpan.FromSeconds(10);
+            Assert.AreEqual(_source.PresentationTime, sut.PresentationTime);
+        }
+        [TestMethod]
+        [TestCategory("SourceConverter")]
+        public void SourceConverter_IsActualChannelCountPassedThrough()
+        {
+            Assert.AreEqual(_source.ActualChannelCount, sut.ActualChannelCount);
+            
+        }
+        [TestMethod]
+        [TestCategory("SourceConverter")]
+        public void SourceConverter_IsActualFrequencyCountPassedThrough()
+        {
+            Assert.AreEqual(_source.ActualFrequencyCount, sut.ActualFrequencyCount);
+        }
+        [TestMethod]
+        [TestCategory("SourceConverter")]
+        public void SourceConverter_IsActualMinFrequencyPassedThrough()
+        {
+            Assert.AreEqual(_source.ActualMinFrequency, sut.ActualMinFrequency);
+        }
+
+        [TestMethod]
+        [TestCategory("SourceConverter")]
+        public void SourceConverter_IsActualMaxFrequencyPassedThrough()
+        {
+            Assert.AreEqual(_source.ActualMaxFrequency, sut.ActualMaxFrequency);
+        }
+
+        [TestMethod]
+        [TestCategory("SourceConverter")]
+        public void SourceConverter_IsActualFrequencyScalePassedThrough()
+        {
+            Assert.AreEqual(_source.ActualFrequencyScale, sut.ActualFrequencyScale);
+        }
+
+        [TestMethod]
+        [TestCategory("SourceConverter")]
+        public void SourceConverter_SettingFrequencyCountToZeroThrows()
+        {
+            Assert.ThrowsException<ArgumentException>(() => { sut.FrequencyCount = 0; });
+        }
+        [TestMethod]
+        [TestCategory("SourceConverter")]
+        public void SourceConverter_SettingChannelCountToZeroThrows()
+        {
+            Assert.ThrowsException<ArgumentException>(() => { sut.ChannelCount = 0; });
+        }
+
+        [TestMethod]
+        [TestCategory("SourceConverter")]
+        public void SourceConverter_SettingMinFrequencyToZeroWithLogScaleThrows()
+        {
+            sut.FrequencyScale = ScaleType.Logarithmic;
+            Assert.ThrowsException<ArgumentException>(() => { sut.MinFrequency = 0.0f; });
+        }
+        [TestCategory("SourceConverter")]
+        public void SourceConverter_SettingMinFrequencyLTZeroThrows()
+        {
+            sut.FrequencyScale = ScaleType.Logarithmic;
+            Assert.ThrowsException<ArgumentException>(() => { sut.MinFrequency = -1.0f; });
+        }
+
+        [TestMethod]
+        [TestCategory("SourceConverter")]
+        public void SourceConverter_SettingMaxFrequencyLTMinThrows()
+        {
+            sut.MinFrequency = 20.0f;
+            Assert.ThrowsException<ArgumentException>(() => { sut.MaxFrequency = 10.0f; });
+        }
+        [TestMethod]
+        [TestCategory("SourceConverter")]
+        public void SourceConverter_SettingMinFrequencyGTEMaxThrows()
+        {
+            sut.MaxFrequency = 20000.0f;
+            Assert.ThrowsException<ArgumentException>(() => { sut.MinFrequency = 20000.0f; });
+        }
+
+        [TestMethod]
+        [TestCategory("SourceConverter")]
+        void SourceConverter_GetData()
+        {
+
+            var frame = new VisualizationDataFrame(
+                TimeSpan.FromSeconds(1),
+                TimeSpan.FromTicks(166667),
+                ScalarData.CreateEmpty(2),
+                ScalarData.CreateEmpty(2),
+                SpectrumData.CreateEmpty(2, 1024, ScaleType.Linear, ScaleType.Linear, 0.0f, 22100.0f)
+                );
+            _source.Frame = frame;
+
+            // First step passthrough
+            var f = sut.GetData();
+            Assert.IsNotNull(f);
+            Assert.AreEqual(2, f.Spectrum.Count);
+            Assert.AreEqual(1024u, f.Spectrum.FrequencyCount);
+
+            _source.Frame = null;
+            f = sut.GetData();
+            Assert.IsNotNull(f);
+
+        }
+
+        [TestMethod]
+        [TestCategory("SourceConverter")]
+        void SourceConverter_RiseAndFall()
+        {
+            sut.Source = _source;
+
+            var frame1 = new VisualizationDataFrame(
+                TimeSpan.FromSeconds(1),
+                TimeSpan.FromTicks(166667),
+                ScalarData.CreateEmpty(2),
+                ScalarData.CreateEmpty(2),
+                SpectrumData.CreateEmpty(2, 10, ScaleType.Linear, ScaleType.Linear, 0.0f, 22100.0f)
+                );
+
+            var frame2 = new VisualizationDataFrame(
+                TimeSpan.FromSeconds(2),
+                TimeSpan.FromTicks(166667),
+                ScalarData.Create(new float[] { 0.5f, 1.0f }),
+                ScalarData.Create(new float[] { 1.0f, 0.5f }),
+                SpectrumData.Create(new float[][]
+                {
+                    new float[] { 1.0f,0,0,0,0,0,0,0,0,0 },
+                    new float[] { 0.5f,0,0,0,0,0,0,0,0,0 }
+                }, ScaleType.Linear, ScaleType.Linear, 0.0f, 22100.0f)
+                );
+
+            _source.Frame = frame1;
+            sut.SpectrumRiseTime = TimeSpan.FromMilliseconds(100);
+            sut.SpectrumFallTime = TimeSpan.FromMilliseconds(50);
+            sut.RmsRiseTime = TimeSpan.FromMilliseconds(80);
+            sut.RmsFallTime = TimeSpan.FromMilliseconds(40);
+            sut.PeakRiseTime = TimeSpan.FromMilliseconds(20);
+            sut.PeakFallTime = TimeSpan.FromMilliseconds(200);
+            var data1 = sut.GetData();
+            _source.Frame = frame2;
+            var data2 = sut.GetData();
+            Assert.IsNotNull(data2.RMS);
+            Assert.IsNotNull(data2.Peak);
+            Assert.IsNotNull(data2.Spectrum);
+
+            CollectionAssert.AreEqual(new float[] { 0.09403199f, 0.188063979f }, data2.RMS.ToArray());
+            CollectionAssert.AreEqual(new float[] { 0.5654025f, 0.282701254f }, data2.Peak.ToArray());
+            CollectionAssert.AreEqual(new float[] { 0.1535185f, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, data2.Spectrum[0].ToArray());
+            CollectionAssert.AreEqual(new float[] { 0.07675925f, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, data2.Spectrum[1].ToArray());
+        }
+
+    }
+
+    [TestClass]
+    public class SourceConverterPropertyValidationAfterInit
+    {
+        SourceConverter sut;
+        [TestInitialize]
+        public void TestInit()
+        {
+            sut = new SourceConverter();
+        }
+
         [TestCategory("SourceConverter")]
         [TestMethod()]
+        public void SourceConverter_SetFpsThrowsNotImplemented()
+        {
+            Assert.ThrowsException<NotImplementedException>(() =>
+            {
+                sut.Fps = 61.0f;
+            });
+        }
+        [TestCategory("SourceConverter")]
+        [TestMethod()]
+        public void SourceConverter_GetFpsThrows()
+        {
+            Assert.ThrowsException<COMException>(() =>
+            {
+                var v = sut.Fps;
+            });
+        }
+        [TestCategory("SourceConverter")]
+        [TestMethod()]
+        public void SourceConverter_SetIsSuspendedThrowsNotImplemented()
+        {
+            Assert.ThrowsException<NotImplementedException>(() =>
+            {
+                sut.IsSuspended = false;
+            });
+        }
+
+        [TestCategory("SourceConverter")]
+        [TestMethod()]
+        public void SourceConverter_GetIsSuspendedThrows()
+        {
+            Assert.ThrowsException<COMException>(() =>
+            {
+                var v = sut.IsSuspended;
+            });
+        }
+        [TestCategory("SourceConverter")]
+        [TestMethod()]
+        public void SourceConverter_GetPlaybackStateThrowsNotImplemented()
+        {
+            Assert.ThrowsException<COMException>(() =>
+            {
+                var v = sut.PlaybackState;
+            });
+        }
+        [TestCategory("SourceConverter")]
+        [TestMethod()]
+        public void SourceConverter_GetPlaybackStateThrows()
+        {
+            Assert.ThrowsException<COMException>(() =>
+            {
+                var v = sut.PlaybackState;
+            });
+        }
+        [TestCategory("SourceConverter")]
+        [TestMethod()]
+        public void SourceConverter_GetPresentationTimeThrows()
+        {
+            Assert.ThrowsException<COMException>(() =>
+            {
+                var v = sut.PresentationTime;
+            });
+        }
+
+        [TestCategory("SourceConverter")]
+        [TestMethod()]
+        public void SourceConverter_AnalyzerTypesAreAll()
+        {
+            Assert.AreEqual(AnalyzerType.All, sut.AnalyzerTypes);
+        }
+
+        [TestCategory("SourceConverter")]
+        [TestMethod()]
+        public void SourceConverter_SourceIsNull()
+        {
+            Assert.IsNull(sut.Source);
+        }
+        [TestCategory("SourceConverter")]
+        [TestMethod()]
+        public void SourceConverter_FrequencyCountIsNull()
+        {
+            Assert.IsNull(sut.FrequencyCount);
+        }
+        [TestCategory("SourceConverter")]
+        [TestMethod()]
+        public void SourceConverter_ChannelCountIsNull()
+        {
+            Assert.IsNull(sut.ChannelCount);
+        }
+        [TestCategory("SourceConverter")]
+        [TestMethod()]
+        public void SourceConverter_RmsRiseTimeIsNull()
+        {
+            Assert.IsNull(sut.RmsRiseTime);
+        }
+        [TestCategory("SourceConverter")]
+        [TestMethod()]
+        public void SourceConverter_RmsFallTimeIsNull()
+        {
+            Assert.IsNull(sut.RmsFallTime);
+        }
+        [TestCategory("SourceConverter")]
+        [TestMethod()]
+        public void SourceConverter_PeakRiseTimeIsNull()
+        {
+            Assert.IsNull(sut.PeakRiseTime);
+        }
+        [TestCategory("SourceConverter")]
+        [TestMethod()]
+        public void SourceConverter_PeakFallTimeIsNull()
+        {
+            Assert.IsNull(sut.PeakFallTime);
+        }
+
+        [TestCategory("SourceConverter")]
+        [TestMethod()]
+        public void SourceConverter_SpectrumRiseTimeIsNull()
+        {
+            Assert.IsNull(sut.SpectrumRiseTime);
+        }
+        [TestCategory("SourceConverter")]
+        [TestMethod()]
+        public void SourceConverter_SpectrumFallTimeIsNull()
+        {
+            Assert.IsNull(sut.SpectrumFallTime);
+        }
+        [TestCategory("SourceConverter")]
+        [TestMethod()]
+        public void SourceConverter_MinFrequencyIsNull()
+        {
+            Assert.IsNull(sut.MinFrequency);
+        }
+        [TestCategory("SourceConverter")]
+        [TestMethod()]
+        public void SourceConverter_MaxFrequencyIsNull()
+        {
+            Assert.IsNull(sut.MaxFrequency);
+        }
+        [TestCategory("SourceConverter")]
+        [TestMethod()]
+        public void SourceConverter_FrequencyScaleIsNull()
+        {
+            Assert.IsNull(sut.FrequencyScale);
+        }
+
+
         public void SourceConverter_props()
         {
-            SourceConverter converter = new SourceConverter();
-            Assert.ThrowsException<Exception>(() =>
-            {
-                var v = converter.Fps;
-            });
-            Assert.ThrowsException<Exception>(() =>
-            {
-                var v = converter.IsSuspended;
-            });
-            Assert.ThrowsException<Exception>(() =>
-            {
-                var v = converter.PlaybackState;
-            });
-            Assert.ThrowsException<Exception>(() =>
-            {
-                var v = converter.PresentationTime;
-            });
-            Assert.AreEqual(AnalyzerType.All, converter.AnalyzerTypes);
-            Assert.IsNull(converter.Source);
-            Assert.IsNull(converter.FrequencyCount);
-            Assert.IsNull(converter.ChannelCount);
-            Assert.IsNull(converter.SpectrumRiseTime);
-            Assert.IsNull(converter.SpectrumFallTime);
-            Assert.IsNull(converter.RmsRiseTime);
-            Assert.IsNull(converter.RmsFallTime);
-            Assert.IsNull(converter.PeakRiseTime);
-            Assert.IsNull(converter.PeakFallTime);
-            Assert.IsNull(converter.MinFrequency);
-            Assert.IsNull(converter.MaxFrequency);
-            Assert.IsNull(converter.FrequencyScale);
+            var converter = new SourceConverter();
 
             List<string> propertiesChanged = new List<string>();
 
@@ -263,8 +605,6 @@ namespace AudioVisualizer.test
         }
 
 
-        [TestCategory("SourceConverter")]
-        [TestMethod()]
         public void SourceConverter_GetData()
         {
             SourceConverter converter = new SourceConverter();
@@ -293,8 +633,6 @@ namespace AudioVisualizer.test
 
         }
 
-        [TestCategory("SourceConverter")]
-        [TestMethod()]
         public void SourceConverter_RiseAndFall()
         {
             SourceConverter converter = new SourceConverter();
--- AudioVisualizer/SourceConverter.cpp
+++ AudioVisualizer/SourceConverter.cpp
@@ -4,13 +4,27 @@
 namespace winrt::AudioVisualizer::implementation
 {
 	template<typename T>
-	bool IsValueReferenceEqual(Windows::Foundation::IReference<T> value1,Windows::Foundation::IReference<T> value2)
+	bool IsValueReferenceEqual(Windows::Foundation::IReference<T> const &value1,Windows::Foundation::IReference<T> const &value2)
 	{
 		if (!value1 && !value2) // Both values are nullptr, no change
 		{
 			return true;
 		}
 		else if (value1 && value2 && value1.Value() == value2.Value())	// No change in value, return
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	template<typename T>
+	bool IsObjectEqual(T const &value1, T const &value2)
+	{
+		if (!value1 && !value2) // Both values are nullptr, no change
+		{
+			return true;
+		}
+		else if (value1 && value2 && winrt::get_abi(value1) == winrt::get_abi(value2))	// No change in value, return
 		{
 			return true;
 		}
@@ -33,6 +47,13 @@ namespace winrt::AudioVisualizer::implementation
     void SourceConverter::Source(AudioVisualizer::IVisualizationSource const& value)
     {
 		std::lock_guard<std::mutex> lock(_lockMutex);
+		if (IsObjectEqual(value,_source)) {
+			return;	// Both values nullptr, no change
+		}
+		else if ((_source && value) && _source == value) {
+
+		}
+
 		if (_source)
 		{
 			_source.ConfigurationChanged(_sourceChanged);
@@ -186,73 +207,104 @@ namespace winrt::AudioVisualizer::implementation
     Windows::Foundation::IReference<Windows::Foundation::TimeSpan> SourceConverter::RmsRiseTime()
     {
 		std::lock_guard<std::mutex> lock(_lockMutex);
-		throw hresult_not_implemented();
+		return _rmsRiseTime;
     }
 
     void SourceConverter::RmsRiseTime(Windows::Foundation::IReference<Windows::Foundation::TimeSpan> const& value)
     {
 		std::lock_guard<std::mutex> lock(_lockMutex);
-		throw hresult_not_implemented();
+		if (IsValueReferenceEqual(value, _rmsRiseTime))
+			return;
+
+		_rmsRiseTime = value;
+		_cachedOutputFrame = nullptr;
+		_configurationChangedEvent(*this, hstring(L"RmsRiseTime"));
+
     }
 
     Windows::Foundation::IReference<Windows::Foundation::TimeSpan> SourceConverter::RmsFallTime()
     {
 		std::lock_guard<std::mutex> lock(_lockMutex);
-		throw hresult_not_implemented();
+		return _rmsFallTime;
     }
 
     void SourceConverter::RmsFallTime(Windows::Foundation::IReference<Windows::Foundation::TimeSpan> const& value)
     {
 		std::lock_guard<std::mutex> lock(_lockMutex);
-		throw hresult_not_implemented();
+		if (IsValueReferenceEqual(value, _rmsFallTime))
+			return;
+
+		_rmsFallTime = value;
+		_cachedOutputFrame = nullptr;
+		_configurationChangedEvent(*this, hstring(L"RmsFallTime"));
     }
 
     Windows::Foundation::IReference<Windows::Foundation::TimeSpan> SourceConverter::PeakRiseTime()
     {
 		std::lock_guard<std::mutex> lock(_lockMutex);
-		throw hresult_not_implemented();
+		return _peakRiseTime;
     }
 
     void SourceConverter::PeakRiseTime(Windows::Foundation::IReference<Windows::Foundation::TimeSpan> const& value)
     {
 		std::lock_guard<std::mutex> lock(_lockMutex);
-		throw hresult_not_implemented();
+		if (IsValueReferenceEqual(value, _peakRiseTime))
+			return;
+
+		_peakRiseTime = value;
+		_cachedOutputFrame = nullptr;
+		_configurationChangedEvent(*this, hstring(L"PeakRiseTime"));
     }
 
     Windows::Foundation::IReference<Windows::Foundation::TimeSpan> SourceConverter::PeakFallTime()
     {
 		std::lock_guard<std::mutex> lock(_lockMutex);
-		throw hresult_not_implemented();
+		return _peakFallTime;
     }
 
     void SourceConverter::PeakFallTime(Windows::Foundation::IReference<Windows::Foundation::TimeSpan> const& value)
     {
 		std::lock_guard<std::mutex> lock(_lockMutex);
-		throw hresult_not_implemented();
-    }
+		if (IsValueReferenceEqual(value, _peakFallTime))
+			return;
+
+		_peakFallTime = value;
+		_cachedOutputFrame = nullptr;
+		_configurationChangedEvent(*this, hstring(L"PeakFallTime"));
+	}
 
     Windows::Foundation::IReference<Windows::Foundation::TimeSpan> SourceConverter::SpectrumRiseTime()
     {
 		std::lock_guard<std::mutex> lock(_lockMutex);
-		throw hresult_not_implemented();
+		return _spectrumRiseTime;
     }
 
     void SourceConverter::SpectrumRiseTime(Windows::Foundation::IReference<Windows::Foundation::TimeSpan> const& value)
     {
 		std::lock_guard<std::mutex> lock(_lockMutex);
-		throw hresult_not_implemented();
+		if (IsValueReferenceEqual(value, _spectrumRiseTime))
+			return;
+
+		_spectrumRiseTime = value;
+		_cachedOutputFrame = nullptr;
+		_configurationChangedEvent(*this, hstring(L"SpectrumRiseTime"));
     }
 
     Windows::Foundation::IReference<Windows::Foundation::TimeSpan> SourceConverter::SpectrumFallTime()
     {
 		std::lock_guard<std::mutex> lock(_lockMutex);
-		throw hresult_not_implemented();
+		return _spectrumFallTime;
     }
 
     void SourceConverter::SpectrumFallTime(Windows::Foundation::IReference<Windows::Foundation::TimeSpan> const& value)
     {
 		std::lock_guard<std::mutex> lock(_lockMutex);
-		throw hresult_not_implemented();
+		if (IsValueReferenceEqual(value, _spectrumFallTime))
+			return;
+
+		_spectrumFallTime = value;
+		_cachedOutputFrame = nullptr;
+		_configurationChangedEvent(*this, hstring(L"SpectrumFallTime"));
     }
 
     AudioVisualizer::VisualizationDataFrame SourceConverter::GetData()
@@ -262,7 +314,12 @@ namespace winrt::AudioVisualizer::implementation
 
     bool SourceConverter::IsSuspended()
     {
-        throw hresult_not_implemented();
+		std::lock_guard<std::mutex> lock(_lockMutex);
+		if (!_source)
+		{
+			throw hresult_error(E_NOT_VALID_STATE, L"Source not set");
+		}
+		return _source.IsSuspended();
     }
 
     void SourceConverter::IsSuspended(bool value)
@@ -272,8 +329,13 @@ namespace winrt::AudioVisualizer::implementation
 
     float SourceConverter::Fps()
     {
-        throw hresult_not_implemented();
-    }
+		std::lock_guard<std::mutex> lock(_lockMutex);
+		if (!_source)
+		{
+			throw hresult_error(E_NOT_VALID_STATE, L"Source not set");
+		}
+		return _source.Fps();
+	}
 
     void SourceConverter::Fps(float value)
     {
@@ -282,47 +344,100 @@ namespace winrt::AudioVisualizer::implementation
 
     AudioVisualizer::AnalyzerType SourceConverter::AnalyzerTypes()
     {
-        throw hresult_not_implemented();
+		return _analyzerTypes;
     }
 
     void SourceConverter::AnalyzerTypes(AudioVisualizer::AnalyzerType const& value)
     {
-        throw hresult_not_implemented();
+		std::lock_guard<std::mutex> lock(_lockMutex);
+		if (_analyzerTypes == value) {
+			return;	// No change
+		}
+		_analyzerTypes = value;
+		_cachedOutputFrame = nullptr;
+		_configurationChangedEvent(*this, L"AnalyzerTypes");
     }
 
     Windows::Foundation::IReference<Windows::Foundation::TimeSpan> SourceConverter::PresentationTime()
     {
-        throw hresult_not_implemented();
-    }
+		std::lock_guard<std::mutex> lock(_lockMutex);
+		if (!_source)
+		{
+			throw hresult_error(E_NOT_VALID_STATE, L"Source not set");
+		}
+		return _source.PresentationTime();
+	}
 
     AudioVisualizer::SourcePlaybackState SourceConverter::PlaybackState()
     {
-        throw hresult_not_implemented();
+		std::lock_guard<std::mutex> lock(_lockMutex);
+		if (!_source)
+		{
+			throw hresult_error(E_NOT_VALID_STATE, L"Source not set");
+		}
+		return _source.PlaybackState();
     }
 
     Windows::Foundation::IReference<uint32_t> SourceConverter::ActualFrequencyCount()
     {
-        throw hresult_not_implemented();
-    }
+		std::lock_guard<std::mutex> lock(_lockMutex);
+		if (_frequencyCount) {
+			return _frequencyCount;
+		}
+		else if (_source) {
+			return _source.ActualFrequencyCount();
+		}
+		return nullptr;
+	}
 
     Windows::Foundation::IReference<uint32_t> SourceConverter::ActualChannelCount()
     {
-        throw hresult_not_implemented();
+		std::lock_guard<std::mutex> lock(_lockMutex);
+		if (_channelCount) {
+			return _channelCount;
+		}
+		else if (_source) {
+			return _source.ActualChannelCount();
+		}
+		return nullptr;
     }
 
     Windows::Foundation::IReference<float> SourceConverter::ActualMinFrequency()
     {
-        throw hresult_not_implemented();
+		std::lock_guard<std::mutex> lock(_lockMutex);
+		if (_minFrequency) {
+			return _minFrequency;
+		}
+		else if (_source) {
+			return _source.ActualMinFrequency();
+		}
+		return nullptr;
+
     }
 
     Windows::Foundation::IReference<float> SourceConverter::ActualMaxFrequency()
     {
-        throw hresult_not_implemented();
-    }
+		std::lock_guard<std::mutex> lock(_lockMutex);
+		if (_maxFrequency) {
+			return _maxFrequency;
+		}
+		else if (_source) {
+			return _source.ActualMaxFrequency();
+		}
+		return nullptr;
+	}
 
     Windows::Foundation::IReference<AudioVisualizer::ScaleType> SourceConverter::ActualFrequencyScale()
     {
-        throw hresult_not_implemented();
+		std::lock_guard<std::mutex> lock(_lockMutex);
+		if (_frequencyScale) {
+			return _frequencyScale;
+		}
+		else if (_source) {
+			return _source.ActualFrequencyScale();
+		}
+		return nullptr;
+
     }
 
     event_token SourceConverter::ConfigurationChanged(Windows::Foundation::TypedEventHandler<AudioVisualizer::IVisualizationSource, hstring> const& handler)
