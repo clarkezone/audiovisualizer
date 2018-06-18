8402c12ae99ddcb28ad73830781a3611ce6742d9
James Clarke <james@clarkezone.net>
2018-06-18 15:52:05 -0700

    Update build script to restore nuget packages

--- buildbinandnuget.bat
+++ buildbinandnuget.bat
@@ -1,4 +1,6 @@
 @echo off
+powershell -command "(New-Object System.Net.WebClient).DownloadFile('http://dist.nuget.org/win-x86-commandline/latest/nuget.exe', 'nuget.exe')" && (
+nuget.exe restore AudioVisualizer.sln && (
 msbuild AudioVisualizer.sln /t:build /p:Configuration=CIBUILD;Platform=x86 && (
 	msbuild AudioVisualizer.sln /t:build /p:Configuration=CIBUILD;Platform=x64 && (
 		Tools\nuget.exe pack AudioAnalyzer.nuspec -o .\package -Version %1 && (
@@ -10,4 +12,10 @@ msbuild AudioVisualizer.sln /t:build /p:Configuration=CIBUILD;Platform=x86 && (
 
 ) || (
 	echo "x86 build failed"
+)
+) || (
+	echo "failed to restore packages"
+)
+) || (
+	echo "failed to get nuget.exe"
 )
