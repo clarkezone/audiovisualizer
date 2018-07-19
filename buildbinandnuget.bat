4731bccfa67b101cdaae02d56a9e35d0cceab6a7
Tõnu Vanatalu <tonuv@hotmail.com>
2018-07-19 12:44:20 +0300

    Fixed build script

--- buildbinandnuget.bat
+++ buildbinandnuget.bat
@@ -1,21 +1,16 @@
 @echo off
-powershell -command "(New-Object System.Net.WebClient).DownloadFile('http://dist.nuget.org/win-x86-commandline/latest/nuget.exe', 'nuget.exe')" && (
-nuget.exe restore AudioVisualizer.sln && (
-msbuild AudioVisualizer.sln /t:build /p:Configuration=CIBUILD;Platform=x86 && (
-	msbuild AudioVisualizer.sln /t:build /p:Configuration=CIBUILD;Platform=x64 && (
-		Tools\nuget.exe pack AudioAnalyzer.nuspec -o .\package -Version %1 && (
-			echo "Package created"
-		) || ( 
-			echo "x64 build failed"
-		)
-	)
 
-) || (
-	echo "x86 build failed"
-)
-) || (
-	echo "failed to restore packages"
-)
-) || (
-	echo "failed to get nuget.exe"
-)
+powershell -command "(New-Object System.Net.WebClient).DownloadFile('http://dist.nuget.org/win-x86-commandline/latest/nuget.exe', 'nuget.exe')" && (
+	nuget.exe restore AudioVisualizer.sln && (
+		msbuild AudioVisualizer.sln /t:build /p:Configuration=Release;Platform=x86 && (
+			vstest.console /Platform:x86 /Framework:FrameworkUAP10 AudioVisualizer.test\bin\x86\Release\AudioVisualizer.test.build.appxrecipe && (
+				msbuild AudioVisualizer.sln /t:build /p:Configuration=Release;Platform=x64 && (
+					vstest.console /Platform:x86 /Framework:FrameworkUAP10 AudioVisualizer.test\bin\x86\Release\AudioVisualizer.test.build.appxrecipe && (
+						Tools\nuget.exe pack AudioAnalyzer.nuspec -o .\package -Version %1 && ( echo "Package created" ) || (echo "Nuget failed")
+					) || ( echo "x64 build failed" )
+				) || ( echo "x64 tests failed")
+			)  || (echo "x86 tests failed" )
+		) || ( echo "x86 build failed" )
+	) || ( echo "failed to restore packages" )
+) || ( echo "failed to get nuget.exe")
+
